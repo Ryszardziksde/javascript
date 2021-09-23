f5e53f4418ec76daf05225c3cef525c953372e18
Ryszardziksde <62832805+Ryszardziksde@users.noreply.github.com>
2021-09-23 13:33:20 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function register(registerform) {
-    if (registerform.password.value==registerform.password2.value)
-        window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-        else
-            prompt("hasła się nie zgadzają");
-}
